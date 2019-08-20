fbc14fb2f5ac5a71ce1edd8dc22483208e707229
Fitra Aziz <fitradevelopers71@gmail.com>
2019-08-20 23:22:04 +0700

    add Landing Page

---
+++ src/components/About.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const About = () => (
+
+	<div>
+		<h2>About Page</h2>
+		<p>Build a website with React, React-Bootstrap, React-Router and Styled-Components</p>
+	</div>
+)
---
+++ src/components/Contact.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Contact = () => (
+
+	<div>
+		<h2>Contact Us</h2>
+		<p>Build a website with React, React-Bootstrap, React-Router and Styled-Components</p>
+	</div>
+)
